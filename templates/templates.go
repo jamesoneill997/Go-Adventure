8d913a0e447fcddbc039ab865db681e71b467f92
jamesoneill997 <jamesoneill997@gmail.com>
2020-11-23 21:21:06 +0000

    added parsing templates

---
+++ templates/templates.go
@@ -0,0 +1,19 @@
+package templates
+
+//Story struct contains list of arcs
+type Story struct {
+	Arcs []Arc `json:"arcs"`
+}
+
+//An Arc type may store a chapter or a character
+type Arc struct {
+	Title   string   `json:"title"`
+	Story   []string `json:"story"`
+	Options []Option `json:"options"`
+}
+
+//Option struct details options and lists next arc
+type Option struct {
+	Text    string `json:"text"`
+	NextArc string `json:"nextArc"`
+}
