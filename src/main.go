4a881c79ad262087f53c851901c57927a9f37c35
jamesoneill997 <jamesoneill997@gmail.com>
2020-11-24 08:25:26 +0000

    added comments

--- src/main.go
+++ src/main.go
@@ -19,16 +19,21 @@ func fileReader(path string) []byte {
 }
 
 func parseStory(arcNames []string, jsonArcs map[string]interface{}) *templates.Story {
-
+	//initialise story and arcs, max size is 25 arcs
 	story := new(templates.Story)
 	arcs := make([]templates.Arc, 25)
 
+	//extract dynamic arc names from json
 	for k := range jsonArcs {
 		arcNames = append(arcNames, k)
 	}
 
+	//for each arc, initialise an arc type and use the map to get nested values
 	for i, val := range arcNames {
+		//construct a json object from map at arc name
 		jsonMap, _ := json.Marshal(jsonArcs[val])
+
+		//handle a file that is too large
 		if len(arcs) > i {
 			json.Unmarshal(jsonMap, &arcs[i])
 		} else {
@@ -40,21 +45,23 @@ func parseStory(arcNames []string, jsonArcs map[string]interface{}) *templates.S
 			arcs = arcs[:i-1]
 		}
 	}
+
+	//write list of arcs to story
 	story.Arcs = arcs
 
 	return story
 }
 
 func main() {
+	//initialise data structs to assist with parsing
 	arcNames := []string{}
-
 	jsonArcs := make(map[string]interface{})
 
+	//handle input file
 	file := fileReader("./gopher.json")
-
 	json.Unmarshal(file, &jsonArcs)
 
+	//parse data to a story object
 	story := parseStory(arcNames, jsonArcs)
-
 	fmt.Println(story.Arcs)
 }
